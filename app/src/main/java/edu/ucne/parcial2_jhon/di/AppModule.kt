c30cbd0affa7f2c8cb802fbc480c0bf6340c37f3
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-01 19:44:56 -0400

    AppModule

---
+++ app/src/main/java/edu/ucne/parcial2_jhon/di/AppModule.kt
@@ -0,0 +1,37 @@
+package edu.ucne.parcial2_jhon.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesApi(moshi: Moshi): /*:Entidad API*/ {
+        return Retrofit.Builder()
+            .baseUrl("/*URL API*/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            //.create(Entidad API::class.java)
+    }
+
+
+}
