6ae87d1485e3ef32f4849c6c4b51775fb3afa814
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-01 20:09:00 -0400

    MainActivity

--- app/src/main/java/edu/ucne/parcial2_jhon/MainActivity.kt
+++ app/src/main/java/edu/ucne/parcial2_jhon/MainActivity.kt
@@ -3,41 +3,35 @@ package edu.ucne.parcial2_jhon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.parcial2_jhon.ui.theme.Parcial2_JhonTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Parcial2_JhonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "HomeScreen"
                 ) {
-                    Greeting("Android")
+
+                    composable("HomeScreen") {
+                        //HomeScreen(navController = navController)
+                    }
+
+                    composable("EditScreen") {
+                        //EditScreen(navController = navController)
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Parcial2_JhonTheme {
-        Greeting("Android")
     }
 }
