f51397911dcf06f779dd1eebb8194694e33919ba
JhonAlbert06 <jhonalbertguzmanrosario@gmail.com>
2022-11-02 18:24:59 -0400

    MainActivity.kt

--- app/src/main/java/edu/ucne/parcial2_jhon/MainActivity.kt
+++ app/src/main/java/edu/ucne/parcial2_jhon/MainActivity.kt
@@ -3,11 +3,19 @@ package edu.ucne.parcial2_jhon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.parcial2_jhon.ui.theme.Parcial2_JhonTheme
+import java.io.IOException
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -15,22 +23,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Parcial2_JhonTheme {
-                val navController = rememberNavController()
 
-                NavHost(
-                    navController = navController,
-                    startDestination = "HomeScreen"
-                ) {
-
-                    composable("HomeScreen") {
-                        //HomeScreen(navController = navController)
-                    }
-
-                    composable("EditScreen") {
-                        //EditScreen(navController = navController)
-                    }
-
+                Box(Modifier.width(50.dp)) {
+                    AsyncImage(
+                        model = "https://example.com/image.jpg",
+                        contentDescription = null
+                    )
                 }
+
+
+
             }
         }
     }
